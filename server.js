f411a67d37d903417acf4eb4e5759869f7614280
Angel <angelSistemas21@gmail.com>
2022-11-03 20:37:13 -0600

    feat: Initial configuration and initilization of the express app

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require ('express');
+const app = express();
+const PORT = 8080;
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+
+app.get('/', (request, response) => {
+    response.send('<h1>MI API</h1>')
+});
+
+app.listen(PORT, () => {
+    console.log(`Running on ${PORT}`);
+});
