cd24f9b8cfb91a629e7196ea90424448d3ed6f0d
Angel <angelSistemas21@gmail.com>
2022-11-04 22:13:23 -0600

    feat: Importing, getting, registering the orders and users models in sequelize as well as setting their relationships

--- config/db.js
+++ config/db.js
@@ -1,8 +1,12 @@
 const { Sequelize } = require('sequelize')
 
+// Importing models
 const productModel = require('../models/product')
 const reviewModel = require('../models/review')
+const userModel = require('../models/user')
+const orderModel = require('../models/order')
 
+// Database connection
 const sequelize = new Sequelize(
     'sesion6',
     'root',
@@ -13,16 +17,20 @@ const sequelize = new Sequelize(
         port: 3306,
         logging: false
     }
-)
+);
 
-const models = [productModel, reviewModel]
+// Getting models
+const models = [productModel, reviewModel, userModel, orderModel]
 
+// Registering models into sequelize
 for (let model of models)
     model(sequelize)
 
 //Configuring relations
 // Se desestructuran pero utilizando el nombre de las tablas que se encuentran en la base de datos
-const { products, reviews } = sequelize.models;
+const { products, reviews, orders, users } = sequelize.models;
 reviews.belongsTo(products); // Relation one-to-one in reviews table
+orders.belongsTo(products);
+orders.belongsTo(users);
 
 module.exports = sequelize
