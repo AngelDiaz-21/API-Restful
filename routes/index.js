f5e1917b8aeb20e2a7a18c2cf591df03370c48cf
Angel <angelSistemas21@gmail.com>
2022-11-06 21:50:48 -0600

    feat: Registration of the routes users, products, reviews and orders

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.use('/users', require('./users'));
+router.use('/products', require('./products'));
+router.use('/reviews', require('./reviews'));
+router.use('/orders', require('./orders'));
+
+module.exports = router;
