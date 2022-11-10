33a92f5daffc1973d1e39103f86f39080a836937
Angel <angelSistemas21@gmail.com>
2022-11-10 14:58:53 -0600

    feat: It is imported and is called helmet and cors

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require ('express');
+const helmet = require ('helmet');
+const cors = require ('cors');
 const app = express();
 const PORT = 8080;
+app.use(helmet());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
@@ -8,7 +12,7 @@ app.get('/', (request, response) => {
     response.send('<h1>MI API</h1>')
 });
 
-app.use('/api', require('./routes'));
+app.use('/api', require('./routes'));//Toma de referencia el archivo index
 
 app.listen(PORT, () => {
     console.log(`Running on ${PORT}`);
