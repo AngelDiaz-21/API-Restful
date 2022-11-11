e1c79f2e0d5b3d5f5c7df64e634b04e3459d0f34
Angel <angelSistemas21@gmail.com>
2022-11-10 18:06:07 -0600

    feat: Authentication middleware implemented

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const authenticate = require('../middlewares/authentication');
 const router = express.Router();
 
 router.use('/auth', require('./auth'));
-router.use('/users', require('./users'));
-router.use('/products', require('./products'));
-router.use('/reviews', require('./reviews'));
-router.use('/orders', require('./orders'));
+router.use('/users', authenticate, require('./users'));
+router.use('/products', authenticate, require('./products'));
+router.use('/reviews', authenticate, require('./reviews'));
+router.use('/orders', authenticate, require('./orders'));
 
 module.exports = router;
